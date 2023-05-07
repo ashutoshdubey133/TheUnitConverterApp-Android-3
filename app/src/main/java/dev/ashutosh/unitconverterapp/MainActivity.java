5f55cc2b41f1e3c5b7dcd37fccffa20a2e389c82
Ashutosh Dubey <ashutoshdubey133@gmail.com>
2023-05-08 01:27:19 +0530

    Added Widgets and Functionalities

--- app/src/main/java/dev/ashutosh/unitconverterapp/MainActivity.java
+++ app/src/main/java/dev/ashutosh/unitconverterapp/MainActivity.java
@@ -3,12 +3,39 @@ package dev.ashutosh.unitconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputValue;
+    Button convertBtn;
+
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputValue = findViewById(R.id.inputValue);
+        convertBtn = findViewById(R.id.convertBtn);
+        result = findViewById(R.id.resultValue);
+
+        convertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double kiloValue = Double.parseDouble(inputValue.getText().toString());
+                double poundValue = convertKilosToPounds(kiloValue);
+
+                result.setText(""+poundValue);
+            }
+        });
+    }
+
+    private double convertKilosToPounds(double kiloValue) {
+        return kiloValue*(2.20462262185);
     }
 }
